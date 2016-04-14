3fa8bbbb76a11c60b44299a53a5cdd0206458d78
Willem Melis <Willem Melis>
2016-04-14 20:49:38 +0200

    visualisation of added features

---
+++ src/ViewLandmarks.py
@@ -0,0 +1,51 @@
+# view the landmarks onto the pictures
+
+import cv2
+import cv2.cv as cv
+import numpy as n
+import readData as rd
+import Teeth as th
+
+import LandMarkSet as lms
+demo1=False
+demo2=True
+
+if demo1==True:
+    test = lms.LandmarkSet(1)
+    img = rd.readRadiograph(1)
+
+    for teeth in range(0,8):
+        for i in range(0,20):
+            cv2.line(img,test.getCoordinates(i*2,teeth),test.getCoordinates(i*2+1,teeth),100,5)
+
+        small = img.copy()
+        small = cv2.resize(small,(700,500))
+
+    cv2.imshow('small',small)
+    cv2.waitKey(0)
+
+if demo2==True:
+    # do procus... anal... -------------
+    listWithLandmarks = [];
+    #read out the first 10 landmarks
+    for i in range(1,11):
+        listWithLandmarks.append(lms.LandmarkSet(i))
+
+    # construct a teeth model
+    t = th.Teeth(1,listWithLandmarks)
+
+    img = rd.readRadiograph(1)
+    #draw line between 2 points to get a striped version of the model
+    for teeth in range(0,8):
+        for i in range(0,20):
+            #print "="+str(t.getNormalizedCoordinatesModelIndividualTeeth(teeth,i*2))
+            cv2.line(img,t.getNormalizedCoordinatesModelIndividualTeeth(teeth,i*2),t.getNormalizedCoordinatesModelIndividualTeeth(teeth,i*2+1),100,5)
+
+
+        small = img.copy()
+        small = cv2.resize(small,(700,500))
+
+        cv2.imshow('small',small)
+        cv2.waitKey(0)
+
+cv2.destroyAllWindows()
